fa1981d50e36929c3b491d130e9d0cf35ce8360b
winkidney <winkidney@gmail.com>
2016-03-19 21:31:05 +0800

    Update: Refine message structure

--- src/smart_qq_bot/messages.py
+++ src/smart_qq_bot/messages.py
@@ -6,64 +6,80 @@ SESS_MSG = "sess_message"
 INPUT_NOTIFY_MSG = "input_notify"
 KICK_MSG = "kick_message"
 
+# Msg type in message content
+OFF_PIC_PART = "offpic"
+C_FACE_PART = "cface"
+
+OFF_PIC_PLACEHOLDER = "[图片]"
+C_FACE_PLACEHOLDER = "[表情]"
+
+
+def mk_msg(msg_dict):
+    msg_map = {
+        GROUP_MSG: GroupMsg,
+        INPUT_NOTIFY_MSG: QMessage,
+        KICK_MSG: QMessage,
+        SESS_MSG: SessMsg,
+        PRIVATE_MSG: PrivateMsg,
+    }
+    return msg_map[msg_dict['pool_type']]
+
 
 class QMessage(object):
 
-    def __init__(self, json_input):
-        self.poll_type = json_input['poll_type']
-        self.from_uin = json_input['value']['from_uin']
-        self.msg_id = json_input['value']['msg_id']
-        self.msg_type = json_input['value']['msg_type']
-        self.to_uin = json_input['value']['to_uin']
+    def __init__(self, msg_dict):
+        self.meta = msg_dict
 
+        self.poll_type = msg_dict['poll_type']
+        value = msg_dict['value']
 
-class MsgWithContent(QMessage):
+        self.from_uin = value['from_uin']
+        self.msg_id = value['msg_id']
+        self.msg_type = value['msg_type']
+        self.to_uin = value['to_uin']
+        self._content = value['content']
+        self.time = value['time']
+        self.font = None
 
-    def __init__(self, json_input):
-        super(MsgWithContent, self).__init__(json_input)
-        self.raw_content = json_input['value']['content']
-        self.content = MsgWithContent.combine_msg(self.raw_content)
-        for i in json_input['value']['content']:
+        for i in value['content']:
             if isinstance(i, list) and i[0] == "font":
                 self.font = i[1]
-        self.time = json_input['value']['time']
 
-    @staticmethod
-    def combine_msg(content):
-        msgtxt = ""
-        for part in content:
-            if type(part) == type(u'\u0000'):
-                msgtxt += part
-            elif len(part) > 1:
-                # 如果是图片
-                if str(part[0]) == "offpic":
-                    msgtxt += "[图片]"
-                elif str(part[0]) == "cface":
-                    msgtxt += "[表情]"
+    @property
+    def content(self):
+        text = ""
+        for msg_part in self._content:
+            if isinstance(msg_part, basestring):
+                text += msg_part
+            elif len(msg_part) > 1:
+                if str(msg_part[0]) == "offpic":
+                    text += ""
+                elif str(text[0]) == "cface":
+                    text += "[表情]"
 
-        return msgtxt
+        return text
 
 
-# 临时会话消息
-class SessMsg(MsgWithContent):
+class SessMsg(QMessage):
+    """
+    临时会话消息
+    """
 
-    def __init__(self, json_input):
-        MsgWithContent.__init__(self, json_input)
-        self.service_type = json_input['value']['service_type']
-        self.id = json_input['value']['id']
-        self.ruin = json_input['value']['ruin']
-        self.flags = json_input['value']['flags']
-
-
-class PmMsg(MsgWithContent):
-
-    def __init__(self, json_input):
-        MsgWithContent.__init__(self, json_input)
+    def __init__(self, msg_dict):
+        super(SessMsg, self).__init__(msg_dict)
+        self.service_type = msg_dict['value']['service_type']
+        self.id = msg_dict['value']['id']
+        self.ruin = msg_dict['value']['ruin']
+        self.flags = msg_dict['value']['flags']
 
 
-class GroupMsg(MsgWithContent):
+class PrivateMsg(QMessage):
+    pass
 
-    def __init__(self, json_input):
-        MsgWithContent.__init__(self, json_input)
-        self.group_code = json_input['value']['group_code']
-        self.send_uin = json_input['value']['send_uin']
+
+class GroupMsg(QMessage):
+
+    def __init__(self, msg_dict):
+        super(GroupMsg, self).__init__(msg_dict)
+        self.group_code = msg_dict['value']['group_code']
+        self.send_uin = msg_dict['value']['send_uin']

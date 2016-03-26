f06fbc83c46d174fb240b9fa0141b87851a6bb94
winkidney <winkidney@gmail.com>
2016-03-26 20:58:23 +0800

    Update: Remove unused data path

--- src/smart_qq_bot/config.py
+++ src/smart_qq_bot/config.py
@@ -10,7 +10,6 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 SMART_QQ_REFER = "http://d1.web2.qq.com/proxy.html?v=20030916001&callback=1&id=2"
 SMART_QQ_URL = "http://w.qq.com/"
 QR_CODE_PATH = "./v.jpg"
-SAVE_DATA_DIR = "./data/tucao_save/"
 
 DEFAULT_PLUGIN_CONFIG = "config/plugin.json"
 

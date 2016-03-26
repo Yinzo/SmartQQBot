ba0eebe526f76fe6db05d8da125fdd575a44da9a
winkidney <winkidney@gmail.com>
2016-03-26 17:40:45 +0800

    Update: Add unittest for is_remove

--- src/plugin_tests/test_satoru.py
+++ src/plugin_tests/test_satoru.py
@@ -9,3 +9,8 @@ class TestSatoru(TestCase):
     def test_is_learn(self):
         result = satoru.is_learn("!learn {1}{2}")
         self.assertEqual(result, ("1", "2"))
+
+    def test_is_remove(self):
+        self.assertEqual(
+            satoru.is_remove("!remove hello"), "hello"
+        )

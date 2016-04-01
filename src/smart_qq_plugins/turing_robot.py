4cf59c20d7dafb772506a83d709b9a093238018a
gucheng <guchengf@gmail.com>
2016-04-01 14:55:15 +0800

    update: support private message

--- src/smart_qq_plugins/turing_robot.py
+++ src/smart_qq_plugins/turing_robot.py
@@ -2,20 +2,19 @@
 from random import randint
 import requests
 
-from smart_qq_bot.signals import on_group_message
+from smart_qq_bot.signals import on_all_message
 
 # 使用前请先前往 http://apistore.baidu.com/apiworks/servicedetail/736.html
 # 申请 API 谢谢
-# 另外需要 request 支持
+# 另外需要 requests 支持
 url = "http://apis.baidu.com/turing/turing/turing"
-payload = "'[5,102]'"
 headers = {
     'apikey': "",
     'cache-control': "no-cache"
 }
 
 
-@on_group_message
+@on_all_message
 def turing_robot(msg, bot):
     """
     :type bot: smart_qq_bot.bot.QQBot
@@ -28,8 +27,13 @@ def turing_robot(msg, bot):
         "userid": ""
     }
 
-    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
     response_json = response.json()
 
     bot.send_qun_msg(msg.from_uin, response_json.get('text'), msg_id=randint(1, 1000))
+
+    if msg.poll_type is 'group_message':
+        bot.send_qun_msg(msg.from_uin, response_json.get('text'), msg_id=randint(1, 1000))
+    else:
+        bot.send_buddy_msg(msg.from_uin, response_json.get('text'), msg_id=randint(1, 1000))

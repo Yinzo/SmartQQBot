37dc1da8c156e851902ca1595263f3403f743ab7
Yinzo <oop995@gmail.com>
2016-04-15 18:27:50 +0800

    Update:在基础插件中改用新的简化回复函数, 唤出插件的回复内容添加随机量

--- src/smart_qq_plugins/basic.py
+++ src/smart_qq_plugins/basic.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+import random
 
 from smart_qq_bot.signals import (
     on_all_message,
@@ -7,25 +8,41 @@ from smart_qq_bot.signals import (
     on_private_message,
 )
 
+# =====唤出插件=====
+
+# 机器人连续回复相同消息时可能会出现
+# 服务器响应成功,但实际并没有发送成功的现象
+# 所以尝试通过随机后缀来尽量避免这一问题
+REPLY_SUFFIX = (
+    '~',
+    '!',
+    '?',
+    '||',
+)
+
 @on_all_message(name='callout')
 def callout(msg, bot):
     if "智障机器人" in msg.content:
-        logging.info("RUNTIMELOG " + str(msg.gounp_code) + " calling me out, trying to reply....")
-        bot.reply_msg(msg, "干嘛（‘·д·）")
+        reply = bot.reply_msg(msg, return_function=True)
+        logging.info("RUNTIMELOG " + str(msg.from_uin) + " calling me out, trying to reply....")
+        reply_content = "干嘛（‘·д·）" + random.choice(REPLY_SUFFIX)
+        reply(reply_content)
 
 
-
+# =====复读插件=====
 class Recorder(object):
-
     def __init__(self):
         self.msg_list = list()
 
 recorder = Recorder()
+
 @on_group_message(name='repeat')
 def repeat(msg, bot):
     global recorder
+    reply = bot.reply_msg(msg, return_function=True)
+
     if len(recorder.msg_list) > 0 and recorder.msg_list[-1].content == msg.content:
         if str(msg.content).strip() not in ("", " ", "[图片]", "[表情]"):
             logging.info("RUNTIMELOG " + str(msg.group_code) + " repeating, trying to reply " + str(msg.content))
-            bot.reply_msg(msg, msg.content)
+            reply(msg.content)
     recorder.msg_list.append(msg)

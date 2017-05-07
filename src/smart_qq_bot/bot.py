e229c0b73f6da651256e82b2d1f9754c50005160
Xuefer <xuefer@gmail.com>
2017-05-07 19:12:22 +0800

    Update: 补全消息拦截的正则 (#122)
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- src/smart_qq_bot/bot.py
+++ src/smart_qq_bot/bot.py
@@ -1005,9 +1005,9 @@ class QQBot(object):
                 .replace("\t", r"\t")
                 )
 
-    injection_escape_regex = re.compile(r"and( *')", re.I);
+    injection_escape_regex = re.compile(r"(\band|\bor|\bxor|(?:^| )&&|(?:^| )\|\|)( +not)?( *'| +[0-9]+ )", re.I);
     def quote(self, content):
-        content = self.injection_escape_regex.sub(r'&\1', content)
+        content = self.injection_escape_regex.sub(r'\1_\2\3', content)
         return self._quote(self._quote(content))
 
     # 发送部分群消息
